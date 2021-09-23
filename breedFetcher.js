e1ef29782e397731dbe9aeb21205dc71100b68dc
Freem11 <freem1985@gmail.com>
2021-09-23 13:58:17 -0700

    hard work

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,27 +1,30 @@
-const request = require("request");
+ const request = require("request");
 
-const input = process.argv.slice(2);
+const fetchBreedDescription = (breed, callback) => {
+    
+  const site = "https://api.thecatapi.com/v1/breeds/search?q=" + breed;
 
-const site = "https://api.thecatapi.com/v1/breeds/search?q=" + input[0];
+  request(site, (error, response, body) => {
 
-// console.log(site)
-const fetcher = function(URl) {
-  request(URl, (error, response, body) => {
-    console.log("error:", error); // Print the error if one occurred
-    console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-    // console.log('body:', body); // Print the HTML for the Google homepage.
-
-    // console.log(body)
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-      console.log("Sorry not cat found");
+    if(error){
+      callback("Connection Error: " + error)
+      return;
+    } 
+    if(response.statusCode >= 400){
+      callback("HTTP Error: " + response.statusCode)
       return;
     }
-    const newData = data[0].description;
 
-    console.log(newData);
-    // console.log(typeof data);
+    const data = JSON.parse(body);
+
+    if (data.length === 0) {
+        callback("Sorry not cat found")
+        return;
+    }
+    callback(null, data[0].description);
+
   });
 };
 
-fetcher(site);
+
+module.exports = {fetchBreedDescription}
